74bae516da9b6fe6982fa58ebc1940512db41714
xingyun <382742279@qq.com>
2024-03-12 22:07:12 +0800

    4

--- zuoye2/src/main/java/com/utils/ShortString_Exception.java
+++ zuoye2/src/main/java/com/utils/ShortString_Exception.java
@@ -2,20 +2,11 @@ package com.utils;
 
 public class ShortString_Exception extends Exception {
 
-    public ShortString_Exception() {
-        super();
-    }
 
     public ShortString_Exception(String message) {
         super(message);
     }
 
-    public ShortString_Exception(String message, Throwable cause) {
-        super(message, cause);
-    }
 
-    public ShortString_Exception(Throwable cause) {
-        super(cause);
-    }
 
 }
